53004fd594305a646975d5af124eb2b81e3c39b7
Frank Mayer <Frank-Mayer@users.noreply.github.com>
2020-02-06 22:35:14 +0100

    Crypt fix

--- share/load.js
+++ share/load.js
@@ -5,12 +5,14 @@ var coverEl = document.getElementById("cover");
 var backgroundEl = document.getElementById("background");
 var buyEl = document.getElementById("buy");
 
-obj = decodeURI(obj).replace("%2B", "+");
+obj = decodeURI(obj).replace("%2B", "+").replace(" ", "+");
+console.log(obj);
 var decrypted = CryptoJS.AES.decrypt(obj, "Secret Passphrase");
 var title = (decrypted.toString(CryptoJS.enc.Utf8))
 console.log(title)
 titleEl.innerHTML = title;
-buyEl.href = "https://www.amazon.de/s?k="+encodeURI(title)+"&i=dvd&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss";
+buyEl.href = "https://www.amazon.de/s?k="+encodeURI(title)+"&i=dvd&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss"
+/*
 try {
     var request = new XMLHttpRequest();
     request.open('GET', googleApi.query(title+" Movie Cover"), true);
@@ -32,3 +34,4 @@ try {
 catch (e) {
     console.error(e);
 }
+*/
