dbab10fed87f9487fb628c96dbd211634fe2016c
Frank Mayer <Frank-Mayer@users.noreply.github.com>
2020-02-22 19:38:25 +0100

    Update colorPref.js

--- colorPref.js
+++ colorPref.js
@@ -1,14 +1,14 @@
 var colorPref = [
     {
         "light":"false",
-        "main":"#AA0000",
+        "main":"#121212",
         "accent":"#F5F5F5",
         "accent1":"#333333",
         "accent2":"#05385B"
     },
     {
         "light":"true",
-        "main":"#AA0000",
+        "main":"#F5F5F5",
         "accent":"#242424",
         "accent1":"#DDDDDD",
         "accent2":"#EEEEEE"
