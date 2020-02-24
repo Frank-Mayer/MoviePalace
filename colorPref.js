4edd20bb8bc7a5a4c51f3f25d54ab63262d88a0e
Frank Mayer <Frank-Mayer@users.noreply.github.com>
2020-02-24 20:52:35 +0100

    Revert "backdrop filter support check"
    This reverts commit 6d6ffc1f29e075c3aaa48031ad16b6a39b98d3c5.

--- colorPref.js
+++ colorPref.js
@@ -1,6 +1,7 @@
 var colorPref = [
     {
         "light":"false",
+        "dark":"#121212",
         "main":"#121212",
         "accent":"#F5F5F5",
         "accent1":"#333333",
@@ -8,6 +9,7 @@ var colorPref = [
     },
     {
         "light":"true",
+        "dark":"#121212",
         "main":"#F5F5F5",
         "accent":"#242424",
         "accent1":"#DDDDDD",
--- load.js
+++ load.js
@@ -41,10 +41,7 @@ switch (theme) {
     case "light":
     case "1":
         document.documentElement.style.setProperty('--main', colorPref[1].main);
-        document.documentElement.style.setProperty('--dark-transp', (String(colorPref[0].main)+"C0"));
-        document.documentElement.style.setProperty('--dark', (String(colorPref[0].main)));
-        document.documentElement.style.setProperty('--light', (String(colorPref[1].main)));
-        document.documentElement.style.setProperty('--light-transp', (String(colorPref[1].main)+"C0"));
+        document.documentElement.style.setProperty('--dark-transp', (String(colorPref[1].dark)+"E0"));
         document.documentElement.style.setProperty('--accent', colorPref[1].accent);
         document.documentElement.style.setProperty('--accent1', colorPref[1].accent1);
         document.documentElement.style.setProperty('--accent2', colorPref[1].accent2);
@@ -54,10 +51,7 @@ switch (theme) {
         theme = "0";
     case "0":
         document.documentElement.style.setProperty('--main', colorPref[0].main);
-        document.documentElement.style.setProperty('--dark-transp', (String(colorPref[0].main)+"C0"));
-        document.documentElement.style.setProperty('--dark', (String(colorPref[0].main)));
-        document.documentElement.style.setProperty('--light', (String(colorPref[1].main)));
-        document.documentElement.style.setProperty('--light-transp', (String(colorPref[1].main)+"C0"));
+        document.documentElement.style.setProperty('--dark-transp', (String(colorPref[0].dark)+"E0"));
         document.documentElement.style.setProperty('--accent', colorPref[0].accent);
         document.documentElement.style.setProperty('--accent1', colorPref[0].accent1);
         document.documentElement.style.setProperty('--accent2', colorPref[0].accent2);
