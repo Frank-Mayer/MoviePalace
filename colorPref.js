6d6ffc1f29e075c3aaa48031ad16b6a39b98d3c5
Frank Mayer <Frank-Mayer@users.noreply.github.com>
2020-02-24 19:28:04 +0100

    backdrop filter support check

--- colorPref.js
+++ colorPref.js
@@ -1,7 +1,6 @@
 var colorPref = [
     {
         "light":"false",
-        "dark":"#121212",
         "main":"#121212",
         "accent":"#F5F5F5",
         "accent1":"#333333",
@@ -9,7 +8,6 @@ var colorPref = [
     },
     {
         "light":"true",
-        "dark":"#121212",
         "main":"#F5F5F5",
         "accent":"#242424",
         "accent1":"#DDDDDD",
