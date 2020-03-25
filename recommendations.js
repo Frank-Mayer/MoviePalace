7ffa29ee68749d8bf099937da92cc435aed105f5
Frank Mayer <Frank-Mayer@users.noreply.github.com>
2020-03-25 22:44:47 +0100

    Update Color Picker

--- recommendations.js
+++ recommendations.js
@@ -1,28 +1,28 @@
 var recommendation = {
     "data":[
         {
-            "from":229,
-            "to":313,
-            "title":"Passengers",
-            "cover":"https://cdn.cinematerial.com/p/297x/cvwsathm/passengers-movie-poster-md.jpg?v=1478198394"
+            "from":324,
+            "to":405,
+            "title":"Cargo",
+            "cover":"https://upload.wikimedia.org/wikipedia/en/7/7e/Cargo2017poster.jpg"
         },
         {
-            "from":229,
-            "to":324,
-            "title":"Paul - Ein Alien auf der Flucht",
-            "cover":"https://de.web.img3.acsta.net/medias/nmedia/18/79/96/08/19660553.jpg"
+            "from":324,
+            "to":403,
+            "title":"Rango",
+            "cover":"https://media-cache.cinematerial.com/p/500x/o6pesrwp/rango-blu-ray-movie-cover.jpg?v=1456710663"
         },
         {
-            "from":229,
-            "to":326,
-            "title":"Knives Out",
-            "cover":"https://media-cache.cinematerial.com/p/500x/xsf7b61u/knives-out-movie-cover.jpg?v=1580267052"
+            "from":324,
+            "to":329,
+            "title":"John Wick",
+            "cover":"https://cdn.cinematerial.com/p/500x/f0sh3dxo/john-wick-british-movie-poster.jpg?v=1456374794"
         },
         {
-            "from":229,
-            "to":328,
-            "title":"Valerian – Die Stadt der tausend Planeten",
-            "cover":"https://de.web.img3.acsta.net/r_1280_720/pictures/17/03/24/14/15/316061.jpg"
+            "from":324,
+            "to":327,
+            "title":"Kingsman - The Golden Circle",
+            "cover":"https://cdn.cinematerial.com/p/297x/eyina6l7/kingsman-the-golden-circle-british-movie-poster-md.jpg?v=1502991973"
         }
     ],
     "count":0,
@@ -41,12 +41,6 @@ recommendation.data.forEach(e => {
         recommendation.html += '<td><div onclick="useRecommendation(\''+escapeHtml(JSON.stringify(e))+'\')" style=\'background-image: url("'+e.cover+'");\'><p>&#160;</p></img></div></td>';
     }
 });
-// recommendation.html += '</tr><tr>';
-// recommendation.data.forEach(e => {
-//     if (e.date.month >= month && e.date.day >= day) {
-//         recommendation.html += '<td><p>'+e.title.replace(/ /g, "<br>")+'</p></td>';
-//     }
-// });
 recommendation.html += '</tr>';
 
 var encRecTitle;
