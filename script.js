830a1898a81574e1242813034afb0263595434b1
Frank Mayer <frank.mayer19072001@outlook.de>
2020-07-07 15:18:41 +0200

    Note field added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function search (q) {
+function search(q) {
     query = q.toLowerCase();
     sort();
     CreateList();
@@ -15,7 +15,7 @@ function escapeHtml(unsafe) {
 
 var isBlur = false;
 
-function doBlur () {
+function doBlur() {
     document.getElementById('topTools').style.filter = 'blur(4px)';
     document.getElementById('topTools').style.WebkitFilter = 'blur(4px)';
     document.getElementById('list-view').style.filter = 'blur(4px)';
@@ -29,7 +29,7 @@ function doBlur () {
     updateDialogSizeFc();
 }
 
-function unblur () {
+function unblur() {
     document.getElementById('topTools').style.filter = 'blur(0)';
     document.getElementById('topTools').style.WebkitFilter = 'blur(0)';
     document.getElementById('list-view').style.filter = 'blur(0)';
@@ -75,11 +75,11 @@ function gridView() {
     document.documentElement.style.setProperty('--typeVis', "collapse");
 }
 
-function detailsID () {
+function detailsID() {
     return (decodeURI(details.id));
 }
 
-function detailView (e) {
+function detailView(e) {
     create = false;
     document.getElementById("detailFavSwitchP").style.visibility = "visible";
     document.getElementById("del").style.visibility = "visible";
@@ -97,6 +97,8 @@ function detailView (e) {
     document.getElementById('detailStatus').value = Number(details.status);
     document.getElementById('detailTyp').value = Number(details.typ);
     document.getElementById("detailWatchCounter").value = details.watchcount;
+    document.getElementById('comment').style.display = "unset";
+    document.getElementById("comment").value = atob(details.comment ? details.comment : "");
     document.getElementById("coverSelector").style.display = "none";
     doBlur();
     document.getElementById('detailView').style.transform = 'translateX(0)';
@@ -105,14 +107,15 @@ function detailView (e) {
 const capitalize = (s) => {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
-  }
+}
 
-function saveDetails () {
-    details.title = capitalize(String(document.getElementById('detailTitle').value).replace('\'','`'));
-    details.group = capitalize(String(document.getElementById('detailGroup').value).replace('\'','`'));
+function saveDetails() {
+    details.title = capitalize(String(document.getElementById('detailTitle').value).replace('\'', '`'));
+    details.group = capitalize(String(document.getElementById('detailGroup').value).replace('\'', '`'));
     details.episode = String(document.getElementById('detailGroupEp').value);
     details.status = String(document.getElementById('detailStatus').value);
     details.typ = String(document.getElementById('detailTyp').value);
+    details.comment = btoa(String(document.getElementById('comment').value));
     details.alpha = createAlphaSeachString(details);
     if (document.getElementById("detailFavSwitch").checked) {
         details.fav = "true";
@@ -125,7 +128,7 @@ function saveDetails () {
         if (details.cover === "") {
             try {
                 var request = new XMLHttpRequest();
-                request.open('GET', googleApi.query(details.title+" Movie Cover"), true);
+                request.open('GET', googleApi.query(details.title + " Movie Cover"), true);
                 request.onload = function () {
                     var data = JSON.parse(this.response);
 
@@ -137,7 +140,7 @@ function saveDetails () {
                     else {
                         RandomApiKey();
                         console.error(data);
-                        details.cover = '../cover/'+details.title+'.jpg';
+                        details.cover = '../cover/' + details.title + '.jpg';
                     }
                     lib.push(details);
                     document.getElementById("detailFavSwitchP").style.visibility = "visible";
@@ -146,15 +149,15 @@ function saveDetails () {
                     document.getElementById("watchCountEditor").style.visibility = "visible";
                     sort();
                     CreateList();
-                    send("insert","lib",JSON.stringify(details));
+                    send("insert", "lib", JSON.stringify(details));
                     document.getElementById(details.id).scrollIntoView(scrollBehavior);
                 }
                 request.send();
             }
             catch (e) {
                 console.error(e);
-                details.cover = '../cover/'+details.title+'.jpg';
-                send("insert","lib",JSON.stringify(details));
+                details.cover = '../cover/' + details.title + '.jpg';
+                send("insert", "lib", JSON.stringify(details));
                 document.getElementById(details.id).scrollIntoView(scrollBehavior);
             }
         }
@@ -166,7 +169,7 @@ function saveDetails () {
             document.getElementById("watchCountEditor").style.visibility = "visible";
             sort();
             CreateList();
-            send("insert","lib",JSON.stringify(details));
+            send("insert", "lib", JSON.stringify(details));
             document.getElementById(details.id).scrollIntoView(scrollBehavior);
         }
     }
@@ -179,7 +182,7 @@ function saveDetails () {
         }
         sort();
         CreateList();
-        send("update","lib",JSON.stringify(details));
+        send("update", "lib", JSON.stringify(details));
         document.getElementById(details.id).scrollIntoView(scrollBehavior);
     }
     create = false;
@@ -187,56 +190,56 @@ function saveDetails () {
     unblur();
 }
 
-function del () {
-    for( var i = 0; i < lib.length; i++){ 
+function del() {
+    for (var i = 0; i < lib.length; i++) {
         if (lib[i].id == detailsID()) {
-            lib.splice(i, 1); 
+            lib.splice(i, 1);
             break;
         }
     }
-    send("delete","lib",JSON.stringify(details));
+    send("delete", "lib", JSON.stringify(details));
     document.getElementById('detailView').style.transform = 'translateX(200%)';
     unblur();
     sort();
     CreateList();
 }
 
-function delWish (e) {
+function delWish(e) {
     if (typeof e == 'string') {
         e = JSON.parse(e);
     }
-    for (var i=0; i<wishlist.length; i++){
+    for (var i = 0; i < wishlist.length; i++) {
         if (wishlist[i].title == e.title) {
-            wishlist.splice(i, 1); 
+            wishlist.splice(i, 1);
             break;
         }
     }
     loadWishlist();
-    send('delete','wishlist',JSON.stringify(e));
+    send('delete', 'wishlist', JSON.stringify(e));
 }
 
-function boughtWish (e) {
+function boughtWish(e) {
     e = JSON.parse(e);
     document.getElementById('wishlistView').style.transform = 'translateX(200%)';
     createDialog();
-    details.title=e["title"];
+    details.title = e["title"];
     document.getElementById("detailTitle").value = details.title;
     startConfetti();
-    setTimeout(()=>{stopConfetti();},3000);
+    setTimeout(() => { stopConfetti(); }, 3000);
     delWish(e);
 }
 
-function shareWish (e) {
+function shareWish(e) {
     e = JSON.parse(e);
     var request = new XMLHttpRequest();
-    request.open('GET', googleApi.query(e.title+" Movie Cover"), true);
+    request.open('GET', googleApi.query(e.title + " Movie Cover"), true);
     request.onload = function () {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 300) {
             if (Number(data.searchInformation.totalResults) > 0) {
                 e.cover = (data.items[0].link).replace("http://", "https://");
                 var cry = encodeURI(btoa(JSON.stringify(e)));
-                send("shareMovieExt",e.title,cry);
+                send("shareMovieExt", e.title, cry);
             }
         }
         else {
@@ -247,13 +250,13 @@ function shareWish (e) {
     request.send();
 }
 
-function addWishToDB () {
+function addWishToDB() {
     var e = {
         "title": document.getElementById("addWish").value
     }
     wishlist.push(e);
     loadWishlist();
-    send("insert","wishlist",JSON.stringify(e));
+    send("insert", "wishlist", JSON.stringify(e));
     document.getElementById("addWish").value = '';
 }
 
@@ -261,7 +264,7 @@ function sort() {
     alphabet = document.getElementById("alphabet");
     switch (document.getElementById('sortSelect').value) {
         case "alpha":
-            for (var i=0; i<lib.length; i++) {
+            for (var i = 0; i < lib.length; i++) {
                 lib[i].alpha = (createAlphaSeachString(lib[i]));
             }
             SortAlpha();
@@ -291,7 +294,7 @@ function sort() {
     delete alphabet;
 }
 
-function createDialog () {
+function createDialog() {
     resetDetails();
     document.getElementById("detailViewHeader").innerHTML = "Hinzufügen";
     document.getElementById("detailFavSwitchP").style.visibility = "collapse";
@@ -301,6 +304,7 @@ function createDialog () {
     document.getElementById('detailCover').src = "./tv.svg";
     document.getElementById('detailTitle').value = "";
     document.getElementById('detailGroup').value = "";
+    document.getElementById('comment').style.display = "none";
     document.getElementById('detailGroupEp').value = "";
     document.getElementById('detailStatus').value = 0;
     document.getElementById('detailTyp').value = 0;
@@ -310,21 +314,21 @@ function createDialog () {
     document.getElementById('detailView').style.transform = 'translateX(0)';
 }
 
-function eventFire(el, etype){
+function eventFire(el, etype) {
     if (el.fireEvent) {
-      el.fireEvent('on' + etype);
+        el.fireEvent('on' + etype);
     } else {
-      var evObj = document.createEvent('Events');
-      evObj.initEvent(etype, true, false);
-      el.dispatchEvent(evObj);
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
     }
-  }
-
-function random () {
-    eventFire(document.getElementById(lib[Math.floor(Math.random() * (lib.length-1))].id), 'click');
 }
 
-function findCover () {
+function random() {
+    eventFire(document.getElementById(lib[Math.floor(Math.random() * (lib.length - 1))].id), 'click');
+}
+
+function findCover() {
     if (document.getElementById("coverSelector").style.display === "block") {
         document.getElementById("coverSelector").scrollTo(0, 0);
         document.getElementById("coverSelector").style.display = "none";
@@ -338,7 +342,7 @@ function findCover () {
                 document.getElementById("coverSelect3").src = "";
 
                 var request = new XMLHttpRequest();
-                request.open('GET', googleApi.query(document.getElementById("detailTitle").value+" Movie Cover"), true);
+                request.open('GET', googleApi.query(document.getElementById("detailTitle").value + " Movie Cover"), true);
                 request.onload = function () {
                     var data = JSON.parse(this.response);
                     if (request.status >= 200 && request.status < 300) {
@@ -375,23 +379,23 @@ function findCover () {
     }
 }
 
-function share () {
+function share() {
     var detailsComp = new Object;
-    detailsComp.title=details.title;
-    detailsComp.cover=details.cover;
+    detailsComp.title = details.title;
+    detailsComp.cover = details.cover;
     var cry = encodeURI(btoa(JSON.stringify(detailsComp)));
-    send("shareMovieExt",details.title,cry);
+    send("shareMovieExt", details.title, cry);
     document.getElementById('detailView').style.transform = 'translateX(200%)'; unblur()
 }
 
 function changeTheme() {
-    send("settings","theme",document.getElementById("themeSelect").value);
+    send("settings", "theme", document.getElementById("themeSelect").value);
     theme = document.getElementById("themeSelect").value;
     applyTheme();
 }
 
 function watchSub() {
-    if (Number(details.watchcount)>0) {
+    if (Number(details.watchcount) > 0) {
         details.watchcount = String(Number(details.watchcount) - 1);
     }
     else {
@@ -407,7 +411,7 @@ function watchAdd() {
 
 function getScrollPercent() {
     var containeR = document.getElementById("list-view");
-    var scrollPercentage = 100 * containeR.scrollTop / (containeR.scrollHeight-containeR.clientHeight);
+    var scrollPercentage = 100 * containeR.scrollTop / (containeR.scrollHeight - containeR.clientHeight);
     return scrollPercentage;
 }
 
@@ -417,7 +421,7 @@ function scrollToLetter(letter) {
     }
     else {
         var floatingChild1 = document.getElementsByClassName("floatingChild1");
-        for (var i=0; i<floatingChild1.length; i++) {
+        for (var i = 0; i < floatingChild1.length; i++) {
             eventFire(floatingChild1[i], 'click');
         }
     }
@@ -425,8 +429,8 @@ function scrollToLetter(letter) {
 
 function shareFullWishList() {
     var strShareFullWishList = '🎬 Meine Wunschliste';
-    for (var i=0; i<wishlist.length; i++) {
-        strShareFullWishList += ('\n'+'•'+wishlist[i].title)
+    for (var i = 0; i < wishlist.length; i++) {
+        strShareFullWishList += ('\n' + '•' + wishlist[i].title)
     }
-    send("sharePlainText","",encodeURI(strShareFullWishList));
+    send("sharePlainText", "", encodeURI(strShareFullWishList));
 }
