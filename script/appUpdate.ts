375cc53de4f1e12724429c7741262313e6b6a9c7
Frank Mayer <frank.mayer19072001@outlook.de>
2020-12-22 23:10:19 +0100

    Navbar bugfix

--- script/appUpdate.ts
+++ script/appUpdate.ts
@@ -1,14 +1,17 @@
 declare const appInfo: {
   version: string;
 };
-if (appInfo && forceNumber(appInfo.version) < 3.1) {
-  confirm(
-    "Ein Update ist verfügbar",
-    "Jetzt herunterladen",
-    "Später erinnern"
-  ).then((v) => {
-    if (v == 1) {
-      window.open("https://frank-mayer.tk/download/PopcornBox.apk", "_blank");
-    }
-  });
+if (appInfo) {
+  const newestVersion = "3.2";
+  if (appInfo && appInfo.version != newestVersion) {
+    confirm(
+      `Ein Update ist verfügbar\nDu hast ${appInfo.version}, aktualisiere jetzt auf ${newestVersion}`,
+      "Jetzt herunterladen",
+      "Später erinnern"
+    ).then((v) => {
+      if (v == 1) {
+        location.href = "https://frank-mayer.tk/download/PopcornBox";
+      }
+    });
+  }
 }
